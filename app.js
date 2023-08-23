5f4ffb11dad3b60606fa49dff8578c312e494ebc
cyberdrow <cbr.drw@ya.ru>
2023-08-23 12:16:48 +0400

    add modded Homework

--- app.js
+++ app.js
@@ -1,24 +1,18 @@
-function moreThanNum(arrItem, num){
-	return arrItem >= num ? true : false;
-	}
-
-function lessThanNum(arrItem, num){
-	return arrItem <= num ? true : false;
-	}
-
+const moreThanNum = (arrItem, num) => arrItem >= num ? true : false;
+const lessThanNum = (arrItem, num) => arrItem <= num ? true : false;
 
 function filterArr(arr, num, fn){
 	let newArr = [];
 	for (let i = 0; i < arr.length; i++ ){
 		let resFn = fn(arr[i], num);
 		if (!resFn) {
-			newArr.push(arr[i])
+			newArr.push(arr[i]);
 		}
 	}
-	return newArr
+	return newArr;
 }		
 
-const arr = [9, 5, 3, 4, 5, 2, 7, 8];
+const arr = [9, 5, 3, 4, 5, 2, 7];
 const num = 5;
 
 console.log(filterArr(arr, num, moreThanNum))
