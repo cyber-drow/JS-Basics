494b9a45c1f60d65212ef9d6c81fe1eacf1bd006
cyberdrow <cbr.drw@ya.ru>
2023-09-19 11:34:57 +0400

    add fixed Homework "12-luna'

--- app.js
+++ app.js
@@ -1,9 +1,7 @@
 const cardNumber = '4561-2612-1234-5464';
 
 const checkLuna = (card) => {
-	
-	const cardArr = card.replaceAll('-', '').split('');
-
+	const cardArr = card.replaceAll('-', '').split('').map((el) => el * 1);
 	const modCard = cardArr.map((el, i) => {
 		if (i % 2 === 0) {
 			if (el * 2 > 9) {
@@ -12,11 +10,9 @@ const checkLuna = (card) => {
 		}
 		return el;
 	});
-
 	const resultLuna = modCard.reduce((acc, valuemodCard) => {
-		return acc += valuemodCard * 1;
+		return acc += valuemodCard;
 	}, 0);
-
 	return resultLuna % 10 === 0 ? true : false;
 }
 
